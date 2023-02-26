e6acd407a682ebfd3d63bedf66db7870384bdf22
Mahamodul Hasan Moon <coddermoon@gmail.com>
2023-02-26 23:15:23 +0600

    auth api make doen

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,3 +1,4 @@
+import apiSlice from "../features/api/apiSlice";
 import authSlice from "../features/auth/authSlice";
 
 const { configureStore } = require("@reduxjs/toolkit");
@@ -5,6 +6,7 @@ const { configureStore } = require("@reduxjs/toolkit");
 const store = configureStore({
 
     reducer: {
+       [apiSlice.reducerPath] : apiSlice.reducer,
     auth : authSlice
     }
 })
